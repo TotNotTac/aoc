4aedebfe89711a14ab5ea24a4b019cc7318fef17
jongs29 <silas.de.jong@hva.nl>
2021-12-01 11:17:18 +0100

    Added a nice library function to read and parse lines from a file

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,19 @@
 module Lib
-    ( someFunc
+    ( readLinesFromInputFile
     ) where
+
+import Data.List
+import System.Directory
+import Text.Printf
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+mapReadLines :: Read a => String -> [a]
+mapReadLines = map read . lines
+
+readLinesFromInputFile :: Read a => FilePath -> IO [a]
+readLinesFromInputFile f = do
+  currentDir <- getCurrentDirectory
+  content <- readFile $ currentDir ++ "/src/inputs/" ++ f
+  return $ mapReadLines content
--- src/Solution1.hs
+++ src/Solution1.hs
@@ -4,6 +4,8 @@ import Data.List
 import System.Directory
 import Text.Printf
 
+import Lib
+
 input = "199\n\
 \200\n\
 \208\n\
@@ -24,31 +26,28 @@ input2 = "607\n\
 \769\n\
 \792"
 
-part1 input =
+part1 :: [Int] -> Int
+part1 =
   sum
-  $ map (\x -> if x > 0 then 1 else 0)
-  $ map (foldl1 (flip (-)) . take 2)
-  $ filter ((>1) . length)
-  $ tails
-  $ map read
-  $ lines input
+  . map (\x -> if x > 0 then 1 else 0)
+  . map (foldl1 (flip (-)) . take 2)
+  . filter ((>1) . length)
+  . tails
 
-part2 input =
+part2 :: [Int] -> Int
+part2 =
   sum
-  $ map (\x -> if x > 0 then 1 else 0)
-  $ map (foldl1 (flip (-)) . take 2)
-  $ filter ((>1) . length)
-  $ tails
-  $ map (sum . take 3)
-  $ filter ((>1) . length)
-  $ tails
-  $ map read
-  $ lines input
+  . map (\x -> if x > 0 then 1 else 0)
+  . map (foldl1 (flip (-)) . take 2)
+  . filter ((>1) . length)
+  . tails
+  . map (sum . take 3)
+  . tails
 
 
 
 solve1 = do
   currentDir <- getCurrentDirectory
   input <- readFile $ currentDir ++ "/src/inputs/1.txt"
-  printf "Part 1: %d\n" $ (part1 input :: Int)
-  printf "Part 2: %d\n" $ (part2 input :: Int)
+  printf "Part 1: %d\n" =<< (part1 <$> readLinesFromInputFile "1.txt")
+  printf "Part 2: %d\n" =<< (part2 <$> readLinesFromInputFile "1.txt")
