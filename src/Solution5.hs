6161f4b68d3a79ad33e0db3ca0bcc828bdc0c184
jongs29 <silas.de.jong@hva.nl>
2021-12-05 13:13:24 +0100

    Wow part2 was easy for once

--- src/Solution5.hs
+++ src/Solution5.hs
@@ -6,8 +6,12 @@ import Data.Bifunctor (bimap, second)
 import Data.List.Split
 import qualified Data.Map as M
 import System.Directory
+import Text.Printf
+
 
 type V2 = (Int, Int)
+
+sumV2 (x1, y1) (x2, y2) = (x1+x2, y1+y2)
 
 data Line
   = Line { from :: V2
@@ -15,22 +19,25 @@ data Line
         }
         deriving (Show)
 
-line = "8,0 -> 0,8"
-
-
 parseCoord :: String -> V2
 parseCoord =
   bimap read read
   . second (drop 1)
   . span (/=',')
 
+parseLine :: String -> Line
+parseLine line =
+  uncurry Line
+  $ bimap parseCoord parseCoord
+  $ second (dropWhile (flip elem splitChars))
+  $ span (not . flip elem splitChars) line
+  where splitChars = " ->" :: String
+
 dropDiagonalLines :: [Line] -> [Line]
 dropDiagonalLines
   = filter (\line -> let (x1, y1) = from line
                          (x2, y2) = to line
                      in x1 == x2 || y1 == y2)
-
-sumV2 (x1, y1) (x2, y2) = (x1+x2, y1+y2)
 
 pointsBetween :: Line -> [V2]
 pointsBetween (Line p1@(x1, y1) p2@(x2, y2))
@@ -54,23 +61,27 @@ countOccurences :: (Eq a, Ord a) => M.Map a Int -> [a] -> M.Map a Int
 countOccurences countMap [] = countMap
 countOccurences countMap (x:xs) = countOccurences (M.insertWith (+) x 1 countMap) xs
 
-part1 = do
+part1 input = length
+              $ M.filter (>= 2)
+              $ countOccurences M.empty
+              $ concat
+              $ map pointsBetween
+              $ dropDiagonalLines
+              $ map parseLine
+              $ lines input
+
+part2 input = length
+              $ M.filter (>= 2)
+              $ countOccurences M.empty
+              $ concat
+              $ map pointsBetween
+              -- All I had to change from part 1 was to keep the diagonal lines lol
+              $ map parseLine
+              $ lines input
+
+solve5 = do
   input <- do
         currentDir <- getCurrentDirectory
         readFile $ currentDir ++ "/src/inputs/" ++ "5.txt"
-  print $ length
-        $ M.filter (>= 2)
-        $ countOccurences M.empty
-        $ concat
-        $ map pointsBetween
-        $ dropDiagonalLines
-        $ map parseLine
-        $ lines input
-
-parseLine :: String -> Line
-parseLine line =
-  uncurry Line
-  $ bimap parseCoord parseCoord
-  $ second (dropWhile (flip elem splitChars))
-  $ span (not . flip elem splitChars) line
-  where splitChars = " ->" :: String
+  printf "Part1: %d\n" $ part1 input
+  printf "Part2: %d\n" $ part2 input
