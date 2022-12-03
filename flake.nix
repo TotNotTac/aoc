19cff97addb212ebcdad3e1a7b01b810e4041891
jongs29 <silas.de.jong@hva.nl>
2022-12-03 21:16:41 +0100

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+
+
+{
+  # inspired by: https://serokell.io/blog/practical-nix-flakes#packaging-existing-applications
+  description = "Advent of code in Haskell on NixOS";
+  inputs.nixpkgs.url = "nixpkgs";
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      });
+    in
+    {
+      overlay = (final: prev: {
+        aoc22 = final.haskellPackages.callCabal2nix "AoC22" ./. {};
+      });
+      packages = forAllSystems (system: {
+         aoc22  = nixpkgsFor.${system}.aoc22;
+      });
+      defaultPackage = forAllSystems (system: self.packages.${system}.aoc22);
+      checks = self.packages;
+      devShell = forAllSystems (system: let haskellPackages = nixpkgsFor.${system}.haskellPackages;
+        in haskellPackages.shellFor {
+          packages = p: [self.packages.${system}.aoc22];
+          withHoogle = true;
+          buildInputs = with haskellPackages; [
+            haskell-language-server
+            ghcid
+            cabal-install
+          ];
+        # Change the prompt to show that you are in a devShell
+        shellHook = "export PS1='\\e[1;34mdev > \\e[0m'";
+        });
+  };
+}
