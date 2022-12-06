6f3a8a360fa168a959401c16bc45481a23dadaee
jongs29 <silas.de.jong@hva.nl>
2022-12-06 10:09:59 +0100

    Day 6

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import qualified Solutions.Solution02 as S2
 import qualified Solutions.Solution03 as S3
 import qualified Solutions.Solution04 as S4
 import qualified Solutions.Solution05 as S5
+import qualified Solutions.Solution06 as S6
 
 import Lib
 import qualified Lib.Parsing as Parsing
@@ -14,6 +15,7 @@ solutions = [ S1.solution
             , S3.solution
             , S4.solution
             , S5.solution
+            , S6.solution
             ]
 
 main :: IO ()
