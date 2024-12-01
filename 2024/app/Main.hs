f295e9b328c71dbdb27fc6c79f41f9bbc2ddf0cf
Silas de Jong <silas.de.jong@cobase.com>
2024-12-01 20:14:39 +0100

    solve day 1

---
+++ 2024/app/Main.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE TupleSections #-}
+
+module Main where
+
+import Control.Arrow ((&&&))
+import Data.List (sort)
+import qualified Data.Map as M
+
+main :: IO ()
+main = do
+    input <- formatData <$> readFile "a.txt"
+    print $ uncurry part1 &&& uncurry part2 $ input
+
+formatData :: String -> ([Int], [Int])
+formatData = unzip . map ((\[x, y] -> (x, y)) . map read . words) . lines
+
+diff :: (Num a) => a -> a -> a
+diff x y = abs $ x - y
+
+part1 :: [Int] -> [Int] -> Int
+part1 xs ys = sum $ zipWith diff (sort xs) (sort ys)
+
+-- part2 :: [Int] -> [Int] -> Int
+part2 :: [Int] -> [Int] -> Int
+part2 xs ys = sum $ map (\x -> maybe 0 (* x) $ M.lookup x countMap) xs
+  where
+    countMap :: M.Map Int Int
+    countMap = M.fromListWith (\a b -> a + b) (map (,1) ys)
